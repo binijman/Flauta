19fd70aed3775b68ab56005ef6cb7a7f8a92e76c
Wouter de Vos <wouter@codaisseur.com>
2015-10-31 16:08:03 +0100

    Remove database from the repo and replace them with a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Flautum.destroy_all
+
+Flautum.create({
+  name: "Quinua white",
+  price: 2.8,
+  description: "",
+  image: "quinua white.jpg"
+})
+
+Flautum.create({
+  name: "Quinua black",
+  price: 2.9,
+  description: "",
+  image: "quinua black.jpg"
+})
+
+Flautum.create({
+  name: "Quinua red",
+  price: 2.95,
+  description: "",
+  image: "quinua red.jpg"
+})
+
+Flautum.create({
+  name: "Causa met quinoa",
+  price: 3.95,
+  description: "",
+  image: "eten4.jpg"
+})
+
+Flautum.create({
+  name: "Dulce met quinoa",
+  price: 2.65,
+  description: "",
+  image: "eten8.jpg"
+})
+
+Flautum.create({
+  name: "Kip met quinoa",
+  price: 2.85,
+  description: "",
+  image: "eten5.jpg"
+})
