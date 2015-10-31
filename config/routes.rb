882c2eaf064c8e00130adee12e131e5df8fd68e0
donjego <jegobesseling@Jegos-MacBook-Air.local>
2015-10-31 11:46:06 +0100

    First version of Comments + some styling of blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :articles
-  resources :users
   resources :flauta
+  resources :users
+  resources :articles do
+    resources :comments
+  end
+
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -56,4 +61,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
