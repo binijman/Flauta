96b5b4a17c171f625eadb1eba3246a377224856b
Wouter de Vos <wouter@codaisseur.com>
2015-10-31 14:20:15 +0100

    Fix conflicts in schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20151031114406) do
-=======
-ActiveRecord::Schema.define(version: 20151031095924) do
->>>>>>> origin/master
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
