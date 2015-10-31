2efaa68d8ad5b749e3dcf77e17c40b9f5c1a7c67
donjego <jegobesseling@Jegos-MacBook-Air.local>
2015-10-31 10:33:15 +0100

    addition of Users & Articles structures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031090018) do
+ActiveRecord::Schema.define(version: 20151031093031) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "post"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "flauta", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +28,13 @@ ActiveRecord::Schema.define(version: 20151031090018) do
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "password"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
